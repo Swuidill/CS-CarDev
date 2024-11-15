895d693f6e2a3bc4b93888ad71afa80fa9dc0628
Weixiang Tang <wtang2904@baysidehighschool.org>
2024-11-15 12:51:10 -0500

    block.js

--- VRPractical5/block.js
+++ VRPractical5/block.js
@@ -5,9 +5,8 @@ class Block{
     this.z = y;
     
     this.obj = document.createElement("a-box");
-    this.obj.setAttribute("src","#skin");
+    this.obj.setAttribute("color","grey");
     this.obj.setAttribute("position",{x:x,y:y,z:z});
-	this.obj.setAttribute("height",10);
     scene.append(this.obj);
 
   }
