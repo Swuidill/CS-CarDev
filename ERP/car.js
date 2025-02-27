8c4014bb5d8ce241cefe45d9c06c71ab7ed6e23b
Weixiang Tang <wtang2904@baysidehighschool.org>
2025-02-27 01:20:24 -0500

    Update car.js

--- ERP/car.js
+++ ERP/car.js
@@ -2,5 +2,18 @@ class Car{
     constructor(x, y, z){
         this.obj = document.createElement("a-box");
         this.obj.setAttribute("position", {x, y, z});
+        this.speed = 0.1;
+        this.moving = false;
+        this.direction = new THREE.Vector3(1, 0, 0);
+        scene.appendChild(this.obj);
     }
-  }
+  
+    update(){
+        if(this.moving) {
+            const pos = this.obj.object3D.position;
+            // Clone direction vector before modification
+            pos.add(this.direction.clone().multiplyScalar(this.speed));
+            this.obj.setAttribute("position", pos);
+        }
+    }
+}
